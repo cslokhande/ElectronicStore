f2abc36aa37cb203c0ddd35112bc9b15fd60f9d4
cslokhande <you@example.com>
2023-04-12 19:21:59 +0530

    impliment pagebale in product

--- src/main/java/com/bikkadit/electronicstore/service/impl/FileServiceImpl.java
+++ src/main/java/com/bikkadit/electronicstore/service/impl/FileServiceImpl.java
@@ -4,10 +4,7 @@ import com.bikkadit.electronicstore.service.FileService;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.UUID;
@@ -25,7 +22,7 @@ public class FileServiceImpl implements FileService {
         String randomId = UUID.randomUUID().toString();
         String filename1 = randomId.concat(filename.substring(filename.lastIndexOf(".")));
 
-        // Fullpath
+        // Full path
         String filePath = path + File.separator + filename1;
 
         // create folder if not created
@@ -34,13 +31,15 @@ public class FileServiceImpl implements FileService {
             f.mkdir();
         }
 
-        // filecopy
+        // file copy
         Files.copy(file.getInputStream(), Paths.get(filePath));
         return filename1;
     }
 
     @Override
     public InputStream getResource(String path, String fileName) throws FileNotFoundException {
-        return null;
+        String fullPath = path + File.separator + fileName;
+        InputStream inputStream = new FileInputStream(fullPath);
+        return inputStream;
     }
 }
