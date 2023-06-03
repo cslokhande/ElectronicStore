bc63de13647decafc6c8672abee65c79b4f69cf8
cslokhande <you@example.com>
2023-06-03 19:24:36 +0530

    swagger impliment

--- src/main/java/com/bikkadit/electronicstore/appConfig/MyMapping.java
+++ src/main/java/com/bikkadit/electronicstore/appConfig/MyMapping.java
@@ -1,4 +1,4 @@
-package com.bikkadit.electronicstore.myConfig;
+package com.bikkadit.electronicstore.appConfig;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
